a831d209bdbd09d47816b46b1b528225fe815300
Agnes van Belle <agnesvanbelle@gmail.com>
2012-11-23 22:20:39 +0100

    d

--- RGB_HSV.m
+++ RGB_HSV.m
@@ -1,14 +1,14 @@
 function [ hsvImageMatrix ] = RGB_HSV( imagePath )
 %RGB to HSV
-RGBmatrix = imread(imagePath);
-hsvImageMatrix = zeros(size(RGBmatrix));
-RminG = RGBmatrix(:,:,1)-RGBmatrix(:,:,2);
-RminB = RGBmatrix(:,:,1)-RGBmatrix(:,:,3);
-GminB = RGBmatrix(:,:,2)-RGBmatrix(:,:,3);
-sumRGB = sum(RGBmatrix,3);
-allOnes = zeros(size(sumRGB))+1.0;
-hsvImageMatrix(:,:,1) = atan((sqrt(3)*double(GminB))./double(RminG+RminB));
-hsvImageMatrix(:,:,2) = allOnes - (double(min(RGBmatrix,[], 3)))./double(sumRGB);
-hsvImageMatrix(:,:,3) = sumRGB/3;
+	RGBmatrix = imread(imagePath);
+	hsvImageMatrix = zeros(size(RGBmatrix));
+	RminG = RGBmatrix(:,:,1)-RGBmatrix(:,:,2);
+	RminB = RGBmatrix(:,:,1)-RGBmatrix(:,:,3);
+	GminB = RGBmatrix(:,:,2)-RGBmatrix(:,:,3);
+	sumRGB = sum(RGBmatrix,3);
+	allOnes = zeros(size(sumRGB))+1.0;
+	hsvImageMatrix(:,:,1) = atan((sqrt(3)*double(GminB))./double(RminG+RminB));
+	hsvImageMatrix(:,:,2) = allOnes - (double(min(RGBmatrix,[], 3)))./double(sumRGB);
+	hsvImageMatrix(:,:,3) = sumRGB/3;
 end
 
--- RGB_rgb.m
+++ RGB_rgb.m
@@ -1,9 +1,9 @@
 function [ convertedImageMatrix ] = RGB_rgb( imagePath )
 % conversion function from RGB to normalized rgb
-RGBmatrix = imread(imagePath);
-denominator = sum(RGBmatrix, 3);
-convertedImageMatrix = double(RGBmatrix)./repmat(double(denominator), [1,1,size(RGBmatrix, 3)]);
-[row, col] = find(isnan(convertedImageMatrix));
-convertedImageMatrix(row,col,:) = 1/3; 
+	RGBmatrix = imread(imagePath);
+	denominator = sum(RGBmatrix, 3);
+	convertedImageMatrix = double(RGBmatrix)./repmat(double(denominator), [1,1,size(RGBmatrix, 3)]);
+	[row, col] = find(isnan(convertedImageMatrix));
+	convertedImageMatrix(row,col,:) = 1/3; 
 end
 
